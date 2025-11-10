4c69049a85420cfa02504624d4418fb3bf4f9eb6
alminsfd <alaminhossaintanvir42@gmail.com>
2025-11-10 13:32:18 +0600

    add successfully statetic section-1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@
 
 import React from 'react';
 import Slider from '../components/Slider';
+import AImodels from '../components/AImodels';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
+            <AImodels></AImodels>
         </div>
     );
 };
